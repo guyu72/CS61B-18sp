49d89aaa4334fde4deb6e676ddee603ad10778cf
GuyuLihua <GuyuLihua@outlook.com>
2024-03-07 00:03:01 +0800

    fix IntList and Ari part

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -14,6 +14,7 @@ public class IntListExercises {
             head.first += c;
             head = head.rest;
         }
+        head.first += c;
     }
 
     /**
@@ -51,7 +52,7 @@ public class IntListExercises {
      */
     public static boolean firstDigitEqualsLastDigit(int x) {
         int lastDigit = x % 10;
-        while (x > 10) {
+        while (x >= 10) {
             x = x / 10;
         }
         int firstDigit = x % 10;
@@ -76,7 +77,9 @@ public class IntListExercises {
         if (currElemIsPrime) {
             lst.first *= lst.first;
         }
+        boolean r = squarePrimes(lst.rest);
+        currElemIsPrime |= r;
 
-        return currElemIsPrime || squarePrimes(lst.rest);
+        return currElemIsPrime;
     }
 }
--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,33 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimes1() {
+        IntList lst = IntList.of(2);
+        boolean res = IntListExercises.squarePrimes(lst);
+        assertEquals("4", lst.toString());
+        assertTrue(res);
+    }
+    @Test
+    public void testSquarePrimes2() {
+        IntList lst = IntList.of(2, 3, 5);
+        boolean res = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 9 -> 25", lst.toString());
+        assertTrue(res);
+    }
+    @Test
+    public void testSquarePrimes3() {
+        IntList lst = IntList.of(4, 6, 8, 289);
+        boolean res = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 6 -> 8 -> 289", lst.toString());
+        assertFalse(res);
+    }
+    @Test
+    public void testSquarePrimes4() {
+        IntList lst = IntList.of(2,6,8);
+        boolean res = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 6 -> 8", lst.toString());
+        assertTrue(res);
+    }
 }
